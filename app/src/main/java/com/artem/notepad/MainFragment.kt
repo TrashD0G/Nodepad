eed57199d0b4389d9d09a2d8e5a36252a2e95ff1
Artem <wblizzard86@gmail.com>
2020-08-12 11:08:45 +0500

    *Исправлена ошибка когда можно было создавать полностью пустые заметки *Добавлена функция проверки ввода для создания заметок *Немного переделан дизайн приложения

--- app/src/main/java/com/artem/notepad/MainFragment.kt
+++ app/src/main/java/com/artem/notepad/MainFragment.kt
@@ -1,15 +1,15 @@
 package com.artem.notepad
 
 import android.os.Bundle
-
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_main.*
+
 
 val NoteList = ArrayList<Note>()//Лист для заметок
 
@@ -27,24 +27,25 @@ class MainFragment : Fragment() {
         recyclerView = rootView.findViewById(R.id.list_recycle_view)
 
         recyclerView.layoutManager = LinearLayoutManager(activity)//Вертикальный список
-        recyclerView.adapter = DataAdapter(NoteList)           //Новый адаптер для установки
+        recyclerView.adapter = DataAdapter(NoteList)              //Новый адаптер для установки
+
+
+        recyclerView.addItemDecoration(DividerItemDecoration(context,LinearLayoutManager.VERTICAL))//Линия разделение
+                                                                                                   //между элементами списка
+
+
 
         return rootView
     }
 
     override fun onStart() {
         super.onStart()
-
-
         Btn_create_note.setOnClickListener{
             (activity as MainActivity).navController.navigate(R.id.action_mainFragment_to_testFragment)//Слушатель на кнопке
                                                                                                        //для переключения между
                                                                                                        //фрагментами
         }
 
-
     }
-
-
 
 }
