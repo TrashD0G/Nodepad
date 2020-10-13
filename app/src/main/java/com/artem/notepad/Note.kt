36f6bd260e79e049ded19fc0f93c22e5ae99648f
Artem <wblizzard86@gmail.com>
2020-10-13 20:49:53 +0500

    * Попробовал Data Binding,ViewModel,Lifecycle

--- app/src/main/java/com/artem/notepad/Note.kt
+++ app/src/main/java/com/artem/notepad/Note.kt
@@ -2,7 +2,7 @@ package com.artem.notepad
 
 
 //Класс для создания заметок
-data class Note(val param_head:String, val param_description:String) {
+data class Note(val param_head: String, val param_description: String) {
 
 
 }
